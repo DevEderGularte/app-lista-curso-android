9e192d4a407ba68cec42878562755a759a09980e
gularte <edergularte@gmail.com>
2024-12-10 16:41:56 -0300

    App Lista Curso Android Criação Shered Preferences

--- app/src/main/java/devandroid/edergularte/applistacurso/controller/PessoaController.java
+++ app/src/main/java/devandroid/edergularte/applistacurso/controller/PessoaController.java
@@ -1,4 +1,20 @@
 package devandroid.edergularte.applistacurso.controller;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import devandroid.edergularte.applistacurso.model.Pessoa;
+
 public class PessoaController {
+    @NonNull
+    @Override
+    public String toString() {
+        Log.d("MVC_Controller","Controller Iniciada");
+        return super.toString();
+    }
+
+    public void salvar(Pessoa pessoa) {
+        Log.d("MVC_Controller","Salvo: "+pessoa.toString());
+    }
 }
